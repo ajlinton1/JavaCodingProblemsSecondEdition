b59d519f52cd84f86bb4192ec557343e23f1045a
ajlinton1 <ajlinton@hotmail.com>
2024-02-01 05:44:32 -0800

    Junit

---
+++ src/test/java/chapter1/problem1.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class problem1 {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
